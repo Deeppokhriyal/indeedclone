df1a2926b7dbe0922629c7ec35069a62e5f86211
Deeppokhriyal <dpokhriyal624@gmail.com>
2025-01-30 12:11:50 +0530

    uploaded

---
+++ android/app/src/main/kotlin/com/example/indeed/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.indeed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/job_data.dart
@@ -0,0 +1,28 @@
+class jobdata{
+
+  static List<Map<String, dynamic>> jobs = [
+    {
+      'title': 'Multi Sports Coach',
+      'company': 'Sports Guru India',
+      'location': 'Vikasnagar, Dehradun, Uttarakhand',
+      'salary': '₹15,000 - ₹20,000 a month',
+      'type': 'Morning shift',
+      'label': 'Hiring multiple candidates',
+      'status': 'Fresher',
+      'active': 'Active 15 days ago',
+      'isNew': false,
+    },
+    {
+      'title': 'Office Administrator/Estimator',
+      'company': 'Karma Staff',
+      'location': 'Dehradun, Uttarakhand',
+      'salary': '₹18,000 - ₹25,000 a month',
+      'type': 'Full-time',
+      'label': 'New',
+      'status': 'Typically responds within 3 days',
+      'active': '',
+      'isNew': true,
+    },
+  ];
+
+}
